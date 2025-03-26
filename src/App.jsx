e7273150bf6a597171292f06955bea666532e56b
vermilt <canmilton@gmail.com>
2025-03-26 16:00:50 +0100

    Questionnaire added to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import questions from './data/questions'
+import Questionnaire from './components/Questionnaire'
 
 function App() {
   
 
   return (
     <>
-      <h1>What Cam?</h1>
+      <h1>What Camera?</h1>
+      
+      <Questionnaire/>
     </>
   )
 }
